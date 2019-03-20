e734554a5236e98c0a78613bcc60427d51e2a129
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2019-03-20 16:48:59 -0300

    removed and clear function added

--- Data_Structures/linked_list.c
+++ Data_Structures/linked_list.c
@@ -54,7 +54,7 @@ struct LinkedList{
 	// the last, set it's pointer to null, free memory in the
 	// last pointer and set the last as the iterator pointer
 
-	// But, either case, this is a linked list so......
+	// But, either case, this is a linked list so...
 	
 	int size(){
 		
@@ -62,31 +62,106 @@ struct LinkedList{
 
 	}
 
-};
+	// Iterative search of linked list
 
-// Iterative search of linked list
-
-void explore(const	LinkedList * l){
+	void explore(){
 	
-	Node * p = l->begin;
-	int counter = 0;
+		Node * iterator = begin;
+		int counter = 0;
 
-	// While the current node doesn't point to NULL
-	// (remember the end of the list, which contains an
-	// null pointer, has NO VALUE, therefore don't display
+		// While the current node doesn't point to NULL
+		// (remember the end of the list, which contains an
+		// null pointer, has NO VALUE, therefore don't display
 	
-	// Couldn't think of a way to do without occupying less than
-	// sizeof(LinkedList) + sizeof(Node) * (NUMBER OF ELEMENTS + 1)
-	// memory 
+		// Couldn't think of a way to do without occupying less than
+		// sizeof(LinkedList) + sizeof(Node) * (NUMBER OF ELEMENTS + 1)
+		// memory 
 
-	while(p->pointer != NULL){
+		while(iterator->pointer != NULL){
 
-		printf("Position: %d | Value: %d\n", counter++, p->value);
-		p = p->pointer;
+			printf("Position: %d | Value: %d\n", 1 + counter++, iterator->value);
+			iterator = iterator->pointer;
 	
+		}
+
+	}	
+
+	// Removes item, 1 based indexing
+
+	void remove(int position){
+
+		if(position < 1 or position > v_size){
+				
+			printf("Out of bounds\n");
+			return;
+
+		}
+
+
+		if(position == 1){
+			
+			Node * second = begin->pointer;
+			free(begin);
+			begin = second;
+			v_size--;
+
+			printf("First item successfully removed\n");
+			return;
+
+		}
+
+		int counter = 0;
+		Node * iterator = begin;
+
+		while(iterator->pointer != NULL){
+			
+			counter++;
+
+			if(counter == position - 1){
+				
+				Node * to_free = iterator->pointer;
+				iterator->pointer = iterator->pointer->pointer;
+				free(to_free);
+				
+				if(position == v_size - 1)end = iterator;
+				
+				v_size--;
+
+				printf("Item %d successfully removed\n", position);
+				return;
+
+			}
+
+			iterator = iterator->pointer;
+				
+		}
+
+		printf("FAILED\n");
+
 	}
 
-}
+	// Free memory ocupied by list and resets it to empty
+
+	void clear(){
+	
+		Node * iterator = begin;
+
+		while(iterator->pointer != NULL){
+
+			Node * next = iterator->pointer;
+			free(iterator);
+			iterator = next;
+
+		}
+
+		v_size = 0;
+
+		begin = end = iterator;
+
+	}
+	
+};
+
 
 int main(){
 
@@ -106,10 +181,20 @@ int main(){
 
 	}
 
-	printf("Exploring linked list\n");
+	// printf("Exploring linked list\n");
 
-	explore(&l);
+	l.explore();
 
-	printf("Linked list size = %d\n",l.size());
+	// printf("Linked list size = %d\n",l.size());
+
+	l.remove(4);
+
+	l.explore();
+
+	l.clear();
+
+	printf("EXPLORING\n");
+
+	l.explore();
 
 }
